3e28b99c96023a535592a85c1c267c7d5fa9cac4
kono-Osi <bimtvcool@gmail.com>
2024-12-03 14:44:47 +0200

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/FlyWeight.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Param
+    {
+        public int h;
+        public Bitmap pic;
+    }
+    public class ParamOneCar : Param
+    {
+        public ParamOneCar()
+        {
+            h = 40;
+            pic = new Bitmap(@"C:\Users\Sidor\OneDrive\Рабочий стол\картинки\828ad903db4e4b08.jpeg");
+        }
+
+    }
+    public class ParamTwoCar : Param
+    {
+        public ParamTwoCar()
+        {
+            h = 60;
+            pic = new Bitmap(@"C:\Users\Sidor\OneDrive\Рабочий стол\картинки\vtxnf.jpg");
+        }
+    }
+    public class Car
+    {
+        public int x, y;     
+        public Param p;      
+        public string Direction { get; set; } 
+
+        public Car()
+        {
+            x = 0;
+            y = 0;
+            Direction = "right";
+        }
+
+        public Car(int x, int y, Param p, string direction)
+        {
+            this.p = p;
+            this.x = x;
+            this.y = y;
+            Direction = direction;
+        }
+
+   
+        public void Move()
+        {
+            if (Direction == "right")
+            {
+                x += 5; 
+            }
+            else if (Direction == "left")
+            {
+                x -= 5;
+            }
+        }
+
+ 
+        public override string ToString()
+        {
+            return $"Car at position: x = {x}, y = {y}, height = {p?.h}, direction = {Direction}";
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+        }
+        Graphics g;
+        ParamOneCar p1 = new ParamOneCar();
+        ParamTwoCar p2 = new ParamTwoCar();
+        Car[] t = new Car[1500];
+        Random r = new Random();
+        int count = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            g.Clear(this.BackColor);
+
+            
+            if (count < t.Length)
+            {
+                Param param = (r.Next(0, 10) % 2 == 0) ? (Param)p1 : (Param)p2;
+                string direction = r.Next(0, 2) == 0 ? "right" : "left"; 
+
+                t[count] = new Car(r.Next(10, 100), r.Next(10, this.ClientSize.Height - 50), param, direction);
+                count++;
+            }
+
+        
+            for (int i = 0; i < count; i++)
+            {
+                if (t[i] != null)
+                {
+                    t[i].Move(); 
+                    g.DrawImage(t[i].p.pic, new Rectangle(t[i].x, t[i].y, t[i].p.h, 20));
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
